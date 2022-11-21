e8146227e867d73c0a71267828c3a2d081b98a4c
xxtriplered <92135435+xxtriplered@users.noreply.github.com>
2022-11-21 18:48:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://indaydram-discordcontrol.glitch.me"]
+const urls = ["https://music-bot-v3.bruhhhstw.repl.co"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
